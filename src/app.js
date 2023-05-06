351774cab54813680e55e0651361c3180eba41ac
princekumarpaswan <prasantandal123@gmail.com>
2023-05-06 14:25:31 +0530

    file structuring

--- src/app.js
+++ src/app.js
@@ -1,25 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import img from "./asset/dish.jpg";
-
-const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src="https://thumbs.dreamstime.com/b/food-delivery-logo-template-vector-icon-illustration-170869600.jpg" />
-            </div>
-
-            <div className="nav-iteam">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+import img from "../asset/dish.jpg";
+import Header from "./components/Header";
 
 
 const restaurants = [
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://thumbs.dreamstime.com/b/food-delivery-logo-template-vector-icon-illustration-170869600.jpg" />
+            </div>
+
+            <div className="nav-iteam">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
