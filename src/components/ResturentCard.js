9a54b44219eb5cd8defc6418496b84ba3c95afbf
princekumarpaswan <prasantandal123@gmail.com>
2023-05-10 21:28:18 +0530

    added more files learned about defauld import and export

--- src/components/ResturentCard.js
+++ src/components/ResturentCard.js
@@ -1,9 +1,11 @@
+import { CON_URL } from "./util/constants";
+
 const ResturentCard = (props) => {
     const { resData } = props
     const { cloudinaryImageId, name, cuisines } = resData?.info;
     return (
         <div className="res-card" >
-            <img className="dish-img" alt="dish" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + cloudinaryImageId} />
+            <img className="dish-img" alt="dish" src={CON_URL + cloudinaryImageId} />
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>4.4 starts</h4>
