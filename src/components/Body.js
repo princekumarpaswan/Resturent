bd5d1fb9abe594d7a5deec95124feb659317c516
princekumarpaswan <prasantandal123@gmail.com>
2023-05-16 21:20:42 +0530

    updated main page of the website

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,5 +20,22 @@ const Body = () => {
     )
 }
 
+const Bodyy = () => {
+    return (
+        <div className="body">
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    console.log("prince");
+                }} >Top Rated Restaurants</button>
+            </div>
+            <div className="search" >search</div>
+            <div className="res-container">
+                {
+                    restaurants.map(restu => <ResturentCard key={restu.info.id} resData={restu} />)
+                }
+            </div>
+        </div>
+    )
+}
 
 export default Body
